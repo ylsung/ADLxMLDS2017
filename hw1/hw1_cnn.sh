e9358d80df449e4ddc377d018376650005d10056
YI LIN SUNG <r06942076@ntu.edu.tw>
2017-10-27 23:32:48 +0800

    add #!/bin/bash to shell script

--- hw1/hw1_cnn.sh
+++ hw1/hw1_cnn.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 python main.py --todo test --data_directory $1 --write_file $2 --cuda --batch_size 256 --epoch 100 --model 'rnn_cnn' --load_folder 'rnn_cnn' 
--- hw1/hw1_rnn.sh
+++ hw1/hw1_rnn.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 python main.py --todo test --data_directory $1 --write_file $2 --cuda --batch_size 256 --epoch 100 --model 'rnn' --load_folder 'rnn' 
