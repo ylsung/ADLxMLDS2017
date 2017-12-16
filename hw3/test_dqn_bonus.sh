829505125688790e8d6329d456ef140f720999d8
YI LIN SUNG <r06942076@ntu.edu.tw>
2017-12-16 17:44:27 +0800

    update test_bonus

--- hw3/test_dqn_bonus.sh
+++ hw3/test_dqn_bonus.sh
@@ -1,1 +1,1 @@
-python3 main_dqn_bonus.py --test_dqn_db 'ADLxMLDS2017_hw3_model/dqn_db_henorm_replay10000_target50_exp2000_lstart1000_freq4' --modeL_id '-1'
+python3 main_dqn_bonus.py --test_dqn_duel --load_path 'ADLxMLDS2017_hw3_model/dqn_duel_henorm_replay10000_target50_exp2000_lstart1000_freq4' --model_id '-1'
