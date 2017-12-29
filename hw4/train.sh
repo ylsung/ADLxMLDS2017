2f0cf49cc0e69b81eba7d033befc7cc600d563f0
YI LIN SUNG <r06942076@ntu.edu.tw>
2017-12-30 01:07:40 +0800

    handle conflict

--- hw4/train.sh
+++ hw4/train.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 python3.6 main.py --save 'test' --load '' --nz 100
-=======
-python3.6 main.py --save 'binary_style_mask_aug_z100' --load '' --nz 100
->>>>>>> 140a3e7768061016118ce7205ca72a07947d63b1
+
